c40c4ea099b4910c0d8206282b24c8db48043797
Gavin Zhao <git@gzgz.dev>
2024-01-20 11:00:02 -0500

    Add HIP_CLANG_LAUNCHER for launching Clang through a wrapper executable
    Signed-off-by: Gavin Zhao <git@gzgz.dev>

--- amd/hipcc/src/hipBin_amd.h
+++ amd/hipcc/src/hipBin_amd.h
@@ -878,6 +878,11 @@ void HipBinAmd::executeHipCCCmd(vector<string> argv) {
   string compiler;
   compiler = getHipCC();
   string CMD = compiler;
+
+  if (!var.hipClangLauncher_.empty()) {
+    CMD = "\"" + var.hipClangLauncher_ + "\" \"" + compiler + "\"";
+  }
+
   if (needCFLAGS) {
     CMD += " " + HIPCFLAGS;
   }
